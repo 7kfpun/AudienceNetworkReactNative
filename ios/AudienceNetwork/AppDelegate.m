aa3d8b1c131f4d703f68333a8d482c6d38afa90c
kf <7kfpun@gmail.com>
2017-06-03 11:30:04 +0800

    Change app name

--- ios/AudienceNetwork/AppDelegate.m
+++ ios/AudienceNetwork/AppDelegate.m
@@ -32,8 +32,9 @@
                                                       moduleName:@"AudienceNetwork"
                                                initialProperties:nil
                                                    launchOptions:launchOptions];
-  rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
-
+  // rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
+  rootView.backgroundColor = [[UIColor alloc] initWithRed:0.93f green:0.94f blue:0.95f alpha:1.0];
+  
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
   rootViewController.view = rootView;
